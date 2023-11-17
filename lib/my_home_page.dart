4f507f195a0f368e7f002dffcd31767ab91724a4
Edwin <edwinshakyden@gmail.com>
2023-11-17 12:25:29 +0700

    Update to match Flutter codelab screens

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,49 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'article_screen.dart';
+import 'news_data.dart';
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+  const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        appBar: AppBar(
+            title: const Text('Top Stories'),
+            centerTitle: false,
+            titleTextStyle: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)),
+        body: ListView.separated(
+          separatorBuilder: (context, idx) {
+            return const Divider();
+          },
+          itemCount: getNewsStories().length,
+          itemBuilder: (context, idx) {
+            final article = getNewsStories()[idx];
+            return ListTile(
+              key: Key('$idx ${article.hashCode}'),
+              title: Text(article.title),
+              subtitle: Text(article.description),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute<ArticleScreen>(
+                    builder: (context) {
+                      return ArticleScreen(article: article);
+                    },
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
